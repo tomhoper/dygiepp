bce980c573415ae2a70d61ae6574d6d86e0b78b2
Dave Wadden <dwadden@cs.washington.edu>
2019-08-21 18:28:58 -0700

    Change ensemble prediction to do args.

--- commands/predict_from_ensemble.py
+++ commands/predict_from_ensemble.py
@@ -152,20 +152,20 @@ def main():
     #                 for k in [0, 1, 2, 3]}
     # trigger_vocab_dirs = [path.join(ensemble_dir, "vocabulary_trig_{0}".format(k)) for k in [0, 1, 2 ,3]]
 
-    trigger_dirs = {k: f"{ensemble_dir}/triger_ensemble_seed_{seed}_{k}/prediction_scores_test"
+    trigger_dirs = {k: f"{ensemble_dir}/triger_ensemble_seed_2_{k}/prediction_scores_test"
                     for k in [0, 1, 2, 3]}
-    trigger_vocab_dirs = [f"{ensemble_dir}/triger_ensemble_seed_{seed}_{k}/vocabulary" for k in [0, 1, 2, 3]]
+    trigger_vocab_dirs = [f"{ensemble_dir}/triger_ensemble_seed_2_{k}/vocabulary" for k in [0, 1, 2, 3]]
 
     # arg_dir = path.join(ensemble_dir, "scores_arg")
     # arg_vocab_dir = path.join(ensemble_dir, "vocabulary_arg")
 
     # TODO(dwadden) change this when finished making arg predictions..
 
-    arg_dir = "/data/dwadden/proj/dygie/dygie-experiments/dwadden/models/model_logs/2019_05_18_base_model/prediction_scores"
-    arg_vocab_dir = "/data/dwadden/proj/dygie/dygie-experiments/dwadden/models/model_logs/2019_05_18_base_model/vocabulary"
+    arg_dir = f"/data/dwadden/proj/dygie/dygie-experiments/dwadden/models/model_logs_camera_ready_v2/events_seed_{seed}/prediction_scores_test"
+    arg_vocab_dir = f"/data/dwadden/proj/dygie/dygie-experiments/dwadden/models/model_logs_camera_ready_v2/events_seed_{seed}/vocabulary"
 
     test_file = "/data/dwadden/proj/dygie/dygie-experiments/datasets/ace-event-tongtao-settings/json/test.json"
-    output_file = path.join(out_dir, f"predictions_{seed}.json")
+    output_file = path.join(out_dir, f"arg_predictions_{seed}.json")
     predict_from_ensemble(trigger_dirs, trigger_vocab_dirs, arg_dir, arg_vocab_dir, test_file, output_file)
 
 
