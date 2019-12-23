28ba524a5107024b13031e588c07b9d27505786b
David Wadden <dave.wadden@gmail.com>
2019-12-22 20:10:47 -1000

    A bunch of cleanup to DyGIE.
    - Formatting / commenting
    - Get rid of joint metrics, which are never used.

--- dygie/models/dygie.py
+++ dygie/models/dygie.py
@@ -1,6 +1,5 @@
 import logging
-from typing import Dict, List, Optional
-import copy
+from typing import Dict, List, Optional, Any
 
 import torch
 import torch.nn.functional as F
@@ -25,7 +24,9 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 @Model.register("dygie")
 class DyGIE(Model):
     """
-    TODO(dwadden) document me.
+    Top-level class implementing the DyGIE information extraction model. Handles the construction
+    and refinement of span representations. Delegates individual tasks to modules which it stores as
+    attributes.
 
     Parameters
     ----------
@@ -34,26 +35,28 @@ class DyGIE(Model):
         Used to embed the ``text`` ``TextField`` we get as input to the model.
     context_layer : ``Seq2SeqEncoder``
         This layer incorporates contextual information for each word in the document.
+    modules : ``Dict[str, Dict[str, Any]]``
+        Dicts with config info on the DyGIE modules - e.g. coref, relation, etc.
     feature_size: ``int``
         The embedding size for all the embedded features, such as distances or span widths.
-    submodule_params: ``TODO(dwadden)``
-        A nested dictionary specifying parameters to be passed on to initialize submodules.
     max_span_width: ``int``
         The maximum width of candidate spans.
-    lexical_dropout: ``int``
+    lexical_dropout: ``float``
         The probability of dropping out dimensions of the embedded text.
+    lstm_dropout: ``float``
+        Dropout probability on LSTM output layer, if applicable.
     initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
         Used to initialize the model parameters.
     regularizer : ``RegularizerApplicator``, optional (default=``None``)
         If provided, will be used to calculate the regularization penalty during training.
-    display_metrics: ``List[str]``. A list of the metrics that should be printed out during model
-        training.
+    display_metrics: ``List[str]``
+        A list of the metrics that should be printed out during model training.
     """
     def __init__(self,
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  context_layer: Seq2SeqEncoder,
-                 modules,  # TODO(dwadden) Add type.
+                 modules: Dict[str, Dict[str, Any]],
                  feature_size: int,
                  max_span_width: int,
                  loss_weights: Dict[str, int],
@@ -66,27 +69,21 @@ class DyGIE(Model):
 
         self._text_field_embedder = text_field_embedder
         self._context_layer = context_layer
-
         self._loss_weights = loss_weights
-        self._permanent_loss_weights = copy.deepcopy(self._loss_weights)
+        self._display_metrics = display_metrics
 
-        # Need to add this line so things don't break. TODO(dwadden) sort out what's happening.
-        modules = Params(modules)
-        self._coref = CorefResolver.from_params(vocab=vocab,
-                                                feature_size=feature_size,
-                                                params=modules.pop("coref"))
-        self._ner = NERTagger.from_params(vocab=vocab,
-                                          feature_size=feature_size,
-                                          params=modules.pop("ner"))
-        self._relation = RelationExtractor.from_params(vocab=vocab,
-                                                       feature_size=feature_size,
-                                                       params=modules.pop("relation"))
-        self._events = EventExtractor.from_params(vocab=vocab,
-                                                  feature_size=feature_size,
-                                                  params=modules.pop("events"))
+        # Create modules for individual tasks.
+        modules = Params(modules)  # The initializers want Params objects instead of Dicts.
+        self._coref = CorefResolver.from_params(
+            vocab=vocab, feature_size=feature_size, params=modules.pop("coref"))
+        self._ner = NERTagger.from_params(
+            vocab=vocab, feature_size=feature_size, params=modules.pop("ner"))
+        self._relation = RelationExtractor.from_params(
+            vocab=vocab, feature_size=feature_size, params=modules.pop("relation"))
+        self._events = EventExtractor.from_params(
+            vocab=vocab, feature_size=feature_size, params=modules.pop("events"))
 
-        # Make endpoint span extractor.
-
+        # Create span extractor.
         self._endpoint_span_extractor = EndpointSpanExtractor(context_layer.get_output_dim(),
                                                               combination="x,y",
                                                               num_width_embeddings=max_span_width,
@@ -94,14 +91,13 @@ class DyGIE(Model):
                                                               bucket_widths=False)
         self._max_span_width = max_span_width
 
-        self._display_metrics = display_metrics
-
+        # Lexical dropout
         if lexical_dropout > 0:
             self._lexical_dropout = torch.nn.Dropout(p=lexical_dropout)
         else:
             self._lexical_dropout = lambda x: x
 
-        # Big gotcha: PyTorch doesn't add dropout to the LSTM's output layer. We need to do this
+        # Gotcha: PyTorch doesn't add dropout to the LSTM's output layer. We need to do this
         # manually.
         if lstm_dropout > 0:
             self._lstm_dropout = torch.nn.Dropout(p=lstm_dropout)
@@ -112,18 +108,32 @@ class DyGIE(Model):
 
     @overrides
     def forward(self,
-                text,
-                spans,
-                ner_labels,
-                coref_labels,
-                relation_labels,
-                trigger_labels,
-                argument_labels,
-                metadata):
+                text: Dict[str, torch.Tensor],
+                spans: torch.Tensor,
+                ner_labels: torch.Tensor,
+                coref_labels: torch.Tensor,
+                relation_labels: torch.Tensor,
+                trigger_labels: torch.Tensor,
+                argument_labels: torch.Tensor,
+                metadata: List[Dict]):
         """
-        TODO(dwadden) change this.
+        Run the forward pass.
+        Model forward pass. Construct and refine spans, make IE predictions, and aggregate.
+
+        Parameters
+        ----------
+        text: ``Dict[str, torch.Tensor]``
+            Tensor representations of the text. The keys are different representations (i.e. BERT).
+        spans: ``torch.Tensor``
+            A tensor of all text spans.
+        ner_labels: ``torch.Tensor``
+        coref_labels: ``torch.Tensor``
+        relation_labels: ``torch.Tensor``
+        trigger_labels: ``torch.Tensor``
+        argument_labels: ``torch.Tensor``
+        metadata: ``List[Dict]``
         """
-        # In AllenNLP, AdjacencyFields are passed in as floats. This fixes it.
+        # In AllenNLP, AdjacencyFields are passed in as floats. This changes them to long.
         relation_labels = relation_labels.long()
         argument_labels = argument_labels.long()
 
@@ -148,7 +158,7 @@ class DyGIE(Model):
         text_mask = util.get_text_field_mask(text).float()
         sentence_group_lengths = text_mask.sum(dim=1).long()
 
-        sentence_lengths = 0*text_mask.sum(dim=1).long()
+        sentence_lengths = 0 * text_mask.sum(dim=1).long()
         for i in range(len(metadata)):
             sentence_lengths[i] = metadata[i]["end_ix"] - metadata[i]["start_ix"]
             for k in range(sentence_lengths[i], sentence_group_lengths[i]):
@@ -159,7 +169,8 @@ class DyGIE(Model):
         # TODO(Ulme) Speed this up by tensorizing
         new_text_embeddings = torch.zeros([text_embeddings.shape[0], max_sentence_length, text_embeddings.shape[2]], device=text_embeddings.device)
         for i in range(len(new_text_embeddings)):
-            new_text_embeddings[i][0:metadata[i]["end_ix"] - metadata[i]["start_ix"]] = text_embeddings[i][metadata[i]["start_ix"]:metadata[i]["end_ix"]]
+            new_text_embeddings[i][0:metadata[i]["end_ix"] - metadata[i]["start_ix"]] =\
+                text_embeddings[i][metadata[i]["start_ix"]:metadata[i]["end_ix"]]
 
         #max_sent_len = max(sentence_lengths)
         #the_list = [list(k+metadata[i]["start_ix"] if k < max_sent_len else 0 for k in range(text_embeddings.shape[1])) for i in range(len(metadata))]
@@ -177,21 +188,13 @@ class DyGIE(Model):
 
         # Shape: (batch_size, num_spans)
         span_mask = (spans[:, :, 0] >= 0).float()
-        # SpanFields return -1 when they are used as padding. As we do
-        # some comparisons based on span widths when we attend over the
-        # span representations that we generate from these indices, we
-        # need them to be <= 0. This is only relevant in edge cases where
-        # the number of spans we consider after the pruning stage is >= the
-        # total number of spans, because in this case, it is possible we might
-        # consider a masked span.
+
+        # See comment in AllenNLP coref code for why this is done.
         # Shape: (batch_size, num_spans, 2)
         spans = F.relu(spans.float()).long()
 
         # Shape: (batch_size, num_spans, 2 * encoding_dim + feature_size)
         span_embeddings = self._endpoint_span_extractor(contextualized_embeddings, spans)
-
-        # TODO(Ulme) try normalizing span embeddeings
-        #span_embeddings = span_embeddings.abs().sum(dim=-1).unsqueeze(-1)
 
         # Make calls out to the modules to get results.
         output_coref = {'loss': 0}
@@ -199,39 +202,32 @@ class DyGIE(Model):
         output_relation = {'loss': 0}
         output_events = {'loss': 0}
 
-        # Prune and compute span representations for coreference module
+        # Prune and compute span representations for coreference and relation.
         if self._loss_weights["coref"] > 0 or self._coref.coref_prop > 0:
             output_coref, coref_indices = self._coref.compute_representations(
                 spans, span_mask, span_embeddings, sentence_lengths, coref_labels, metadata)
-
-        # Prune and compute span representations for relation module
         if self._loss_weights["relation"] > 0 or self._relation.rel_prop > 0:
             output_relation = self._relation.compute_representations(
                 spans, span_mask, span_embeddings, sentence_lengths, relation_labels, metadata)
 
         # Propagation of global information to enhance the span embeddings
         if self._coref.coref_prop > 0:
-            # TODO(Ulme) Implement Coref Propagation
             output_coref = self._coref.coref_propagation(output_coref)
             span_embeddings = self._coref.update_spans(output_coref, span_embeddings, coref_indices)
-
         if self._relation.rel_prop > 0:
             output_relation = self._relation.relation_propagation(output_relation)
-            span_embeddings = self.update_span_embeddings(span_embeddings, span_mask,
-                output_relation["top_span_embeddings"], output_relation["top_span_mask"],
-                output_relation["top_span_indices"])
+            span_embeddings = self.update_span_embeddings(
+                span_embeddings, span_mask, output_relation["top_span_embeddings"],
+                output_relation["top_span_mask"], output_relation["top_span_indices"])
 
         # Make predictions and compute losses for each module
         if self._loss_weights['ner'] > 0:
             output_ner = self._ner(
                 spans, span_mask, span_embeddings, sentence_lengths, ner_labels, metadata)
-
         if self._loss_weights['coref'] > 0:
             output_coref = self._coref.predict_labels(output_coref, metadata)
-
         if self._loss_weights['relation'] > 0:
             output_relation = self._relation.predict_labels(relation_labels, output_relation, metadata)
-
         if self._loss_weights['events'] > 0:
             # Make the trigger embeddings the same size as the argument embeddings to make
             # propagation easier.
@@ -255,7 +251,6 @@ class DyGIE(Model):
         if "loss" not in output_relation:
             output_relation["loss"] = 0
 
-        # TODO(dwadden) just did this part.
         loss = (self._loss_weights['coref'] * output_coref['loss'] +
                 self._loss_weights['ner'] * output_ner['loss'] +
                 self._loss_weights['relation'] * output_relation['loss'] +
@@ -267,18 +262,10 @@ class DyGIE(Model):
                            events=output_events)
         output_dict['loss'] = loss
 
-        # Check to see if event predictions are globally compatible (argument labels are compatible
-        # with NER tags and trigger tags).
-        # if self._loss_weights["ner"] > 0 and self._loss_weights["events"] > 0:
-        #     decoded_ner = self._ner.decode(output_dict["ner"])
-        #     decoded_events = self._events.decode(output_dict["events"])
-        #     self._joint_metrics(decoded_ner, decoded_events)
-
         return output_dict
 
     def update_span_embeddings(self, span_embeddings, span_mask, top_span_embeddings, top_span_mask, top_span_indices):
         # TODO(Ulme) Speed this up by tensorizing
-
         new_span_embeddings = span_embeddings.clone()
         for sample_nr in range(len(top_span_mask)):
             for top_span_nr, span_nr in enumerate(top_span_indices[sample_nr]):
@@ -290,24 +277,8 @@ class DyGIE(Model):
     @overrides
     def decode(self, output_dict: Dict[str, torch.Tensor]):
         """
-        Converts the list of spans and predicted antecedent indices into clusters
-        of spans for each element in the batch.
-
-        Parameters
-        ----------
-        output_dict : ``Dict[str, torch.Tensor]``, required.
-            The result of calling :func:`forward` on an instance or batch of instances.
-
-        Returns
-        -------
-        The same output dictionary, but with an additional ``clusters`` key:
-
-        clusters : ``List[List[List[Tuple[int, int]]]]``
-            A nested list, representing, for each instance in the batch, the list of clusters,
-            which are in turn comprised of a list of (start, end) inclusive spans into the
-            original document.
+        Invoke decoding functions of individual modules.
         """
-        # TODO(dwadden) which things are already decoded?
         res = {}
         if self._loss_weights["coref"] > 0:
             res["coref"] = self._coref.decode(output_dict["coref"])
@@ -316,7 +287,7 @@ class DyGIE(Model):
         if self._loss_weights["relation"] > 0:
             res["relation"] = self._relation.decode(output_dict["relation"])
         if self._loss_weights["events"] > 0:
-            res["events"] = output_dict["events"]
+            res["events"] = output_dict["events"]  # Event predictions were decoded already.
 
         return res
 
@@ -329,10 +300,6 @@ class DyGIE(Model):
         metrics_ner = self._ner.get_metrics(reset=reset)
         metrics_relation = self._relation.get_metrics(reset=reset)
         metrics_events = self._events.get_metrics(reset=reset)
-        if self._loss_weights["ner"] > 0 and self._loss_weights["events"] > 0:
-            metrics_joint = self._joint_metrics.get_metric(reset=reset)
-        else:
-            metrics_joint = {}
 
         # Make sure that there aren't any conflicting names.
         metric_names = (list(metrics_coref.keys()) + list(metrics_ner.keys()) +
@@ -341,12 +308,12 @@ class DyGIE(Model):
         all_metrics = dict(list(metrics_coref.items()) +
                            list(metrics_ner.items()) +
                            list(metrics_relation.items()) +
-                           list(metrics_events.items()) +
-                           list(metrics_joint.items()))
+                           list(metrics_events.items()))
 
         # If no list of desired metrics given, display them all.
         if self._display_metrics is None:
             return all_metrics
+
         # Otherwise only display the selected ones.
         res = {}
         for k, v in all_metrics.items():
