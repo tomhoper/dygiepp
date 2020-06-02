8b4e2fedaa9d99605e197b51773b470a0b9dac23
Math-QA <math.qa.dataset@gmail.com>
2020-06-02 16:46:43 -0700

    eval_metric

--- eval_metric.py
+++ eval_metric.py
@@ -8,6 +8,7 @@ import sys
 import pandas as pd
 from eval_utils import depparse_base, allpairs_base, get_openie_predictor,get_srl_predictor,allenlp_base_relations, ie_eval
 import pathlib
+from pathlib import Path
 
 """
 Usage:
@@ -17,21 +18,41 @@ python eval_metric.py --pred ../covidpreds/ --gold UnifiedData/covid_anno_par/go
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser() 
+    parser.add_argument('--data_combo',
+                        type=Path,
+                        help='root dataset folder, contains train/dev/test',
+                        required=True)
 
-    parser.add_argument('--pred',
-                        type=str,
-                        help='path to predictions',
+    parser.add_argument('--root',
+                        type=Path,
+                        help='./',
                         required=True)
-    parser.add_argument('--gold',
-                        type=str,
-                        help='path to gold labels',
-                        required=True)
+
+    parser.add_argument('--mech_effect_mode',
+                        action='store_true')
+
+    # parser.add_argument('--pred',
+    #                     type=str,
+    #                     help='path to predictions',
+    #                     required=True)
+    # parser.add_argument('--gold',
+    #                     type=str,
+    #                     help='path to gold labels',
+    #                     required=False)
 
     args = parser.parse_args()
 
-    GOLD_PATH = "gold_par.tsv"
-    GOLD_PATH = pathlib.Path(GOLD_PATH)
-    PREDS_PATH = pathlib.Path(args.pred) / "pred.tsv"
+    if args.mech_effect_mode == True:
+        gold_path = pathlib.Path(args.root) / 'gold' /  'mech_effect' / 'gold_par.tsv'
+        pred_dir = pathlib.Path(args.root) / 'predictions' / args.data_combo / 'mapped' / 'mech_effect' / "pred.tsv"
+
+    if args.mech_effect_mode == False:
+        gold_path = pathlib.Path(args.root) / 'gold' /  'mech' / 'gold_par.tsv'
+        pred_dir = pathlib.Path(args.root) / 'predictions' / args.data_combo / 'mapped' / 'mech' / "pred.tsv"
+
+
+    GOLD_PATH = pathlib.Path(gold_path)
+    PREDS_PATH = pathlib.Path(pred_dir)
 
     golddf = pd.read_csv(GOLD_PATH, sep="\t",header=None, names=["id","text","arg0","arg1","rel","y"])
     golddf = golddf[golddf["y"]=="accept"]
@@ -42,7 +63,7 @@ if __name__ == '__main__':
     predf = pd.read_csv(PREDS_PATH, sep="\t",names=["id","text","arg0","arg1","rel","conf"])
     predf["id"] = predf["id"].str.replace(r'[+]+$','')
     #check prediction label mapping matches the loaded gold file
-    assert predf["rel"].unique()[0] == golddf["rel"].unique()[0]
+    # assert predf["rel"].unique()[0] == golddf["rel"].unique()[0]
     prediction_dict["model preds"] = predf[["id","arg0","arg1"]]
 
 
@@ -69,7 +90,7 @@ if __name__ == '__main__':
             print(k," -- NO PREDICTIONS -- ")
             continue
         for match_metric in ["jaccard","substring"]:
-            corr_pred, precision,recall, F1 = ie_eval(v,golddf,match_metric=match_metric,jaccard_thresh=0.3)
+            corr_pred, precision,recall, F1 = ie_eval(v,golddf,match_metric=match_metric,jaccard_thresh=0.5)
             print('model: {0} metric: {1} precision:{2} recall {3} f1: {4}'.format(k, match_metric, precision,recall, F1))
         print ("****")
 
