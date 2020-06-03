ee8fbdce0b372177071d551b59a2a80d19a33407
Tom Hope <tomh@s2-server4>
2020-06-02 18:58:51 -0700

    merge corpus for new data struct

--- dygie/data/merge_corpus.py
+++ dygie/data/merge_corpus.py
@@ -1,80 +1,90 @@
 import sys
 import os 
 import json
+from pathlib import Path
+import pathlib
+import argparse
+"""
+usage:
+python dygie/data/merge_corpus.py --root ../coviddata/ --dataset_list scierc,chemprot
+"""
 
-DATA_ROOT = "UnifiedData/"
-COMBO_ROOT = "combo/"
+def merge_datasets(rootdir, dataset_list):
 
-def merge_datasets(dataset_list, effect):
-  train_data_list = []
-  dev_data_list = []
-  test_data_list = []
+  combodir = rootdir / "combos"
+  rootdir = rootdir /"UnifiedData"
 
-  if not os.path.exists(COMBO_ROOT):
-    os.mkdir(COMBO_ROOT) 
-  for item in dataset_list:
-    if effect == False:
-      train_path = DATA_ROOT + item + '/mapped/mech/train.json'
-      dev_path = DATA_ROOT + item + '/mapped/mech/dev.json'
-      test_path = DATA_ROOT + item + '/mapped/mech/test.json'
+  for effect in [True,False]:
+    train_data_list = []
+    dev_data_list = []
+    test_data_list = []
+    for item in dataset_list:
+      print(item, effect)
+      if effect == False:
+        train_path = rootdir.joinpath(item).joinpath('mapped/mech/train.json')
+        dev_path = rootdir.joinpath(item).joinpath('mapped/mech/dev.json')
+        test_path = rootdir.joinpath(item).joinpath('mapped/mech/test.json')
+
+      else:
+        train_path = rootdir.joinpath(item).joinpath('mapped/mech_effect/train.json')
+        dev_path = rootdir.joinpath(item).joinpath('mapped/mech_effect/dev.json')
+        test_path = rootdir.joinpath(item).joinpath('mapped/mech_effect/test.json')
+      
+      #reading the data 
+      train_docs = [json.loads(line) for line in open(train_path)]
+      if item == "srl":
+        dev_docs = []
+        test_docs = []
+      else:
+        dev_docs = [json.loads(line) for line in open(dev_path)]
+        test_docs = [json.loads(line) for line in open(test_path)]
+
+      #adding to what we have 
+      print("adding " + str(len(train_docs)) + " train docs, " +  str(len(dev_docs))+ " dev docs " + str(len(test_docs)) + " test docs form " + item)
+      train_data_list = train_data_list + train_docs
+      dev_data_list = dev_data_list + dev_docs
+      test_data_list = test_data_list + test_docs
+
+    if effect:
+      path = combodir.joinpath('_'.join(dataset_list)).joinpath("mapped").joinpath("mech_effect")
     else:
-      train_path = DATA_ROOT + item + '/mapped/mech_effect/train.json'
-      dev_path = DATA_ROOT + item + '/mapped/mech_effect/dev.json'
-      test_path = DATA_ROOT + item + '/mapped/mech_effect/test.json'
-    
-    #reading the data 
-    train_docs = [json.loads(line) for line in open(train_path)]
-    if item == "srl":
-      dev_docs = []
-      test_docs = []
-    else:
-      dev_docs = [json.loads(line) for line in open(dev_path)]
-      test_docs = [json.loads(line) for line in open(test_path)]
+      path = combodir.joinpath('_'.join(dataset_list)).joinpath("mapped").joinpath("mech")
 
-    #adding to what we have 
-    print("adding " + str(len(train_docs)) + " train docs, " +  str(len(dev_docs))+ " dev docs " + str(len(test_docs)) + " test docs form " + item)
-    train_data_list = train_data_list + train_docs
-    dev_data_list = dev_data_list + dev_docs
-    test_data_list = test_data_list + test_docs
-    
-  #
-  if effect:
-    path = os.path.join(COMBO_ROOT, '_'.join(dataset_list), "mapped", "mech_effect")
-  else:
-    path = os.path.join(COMBO_ROOT, '_'.join(dataset_list), "mapped", "mech")
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True) 
 
-  # import pdb; pdb.set_trace()
-  if not os.path.exists(path):
-    os.makedirs(path) 
-  print("Directory '%s' created" %path)
-  output_file_train = open(path + '/train.json', "w")
-  output_file_dev = open(path + '/dev.json', "w")
-  output_file_test = open(path + '/test.json', "w")
+    print("Directory '%s' created" %path)
+    output_file_train = open(path.joinpath('train.json'), "w")
+    output_file_dev = open(path.joinpath('dev.json'), "w")
+    output_file_test = open(path.joinpath('test.json'), "w")
 
-  for item in train_data_list:
-    json.dump(item, output_file_train)
-    output_file_train.write('\n')
+    for item in train_data_list:
+      json.dump(item, output_file_train)
+      output_file_train.write('\n')
 
+    for item in dev_data_list:
+      json.dump(item, output_file_dev)
+      output_file_dev.write('\n')
 
-  for item in dev_data_list:
-    json.dump(item, output_file_dev)
-    output_file_dev.write('\n')
-
-
-  for item in test_data_list:
-    json.dump(item, output_file_test)
-    output_file_test.write('\n')
+    for item in test_data_list:
+      json.dump(item, output_file_test)
+      output_file_test.write('\n')
 
 
 if __name__ == '__main__':
 
-  import argparse
 
   parser = argparse.ArgumentParser(description='Process some integers.')
   parser.add_argument('--dataset_list',type=str, required=True)
+  parser.add_argument('--root',
+                        type=Path,
+                        help='path to data dir root',
+                        default='../coviddata',
+                        required=True)
+
   args = parser.parse_args()
-  merge_datasets(args.dataset_list.split(','), effect=True)
-  merge_datasets(args.dataset_list.split(','), effect=False)
+  rootdir = args.root
+
+  merge_datasets(rootdir, args.dataset_list.split(','))
 
 
     # merging_list = sys.argv[1:]
