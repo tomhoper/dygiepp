0038ffafce2adadd6b2293591003797e60c91329
Dave Wadden <dwadden@cs.washington.edu>
2020-06-18 16:26:04 -0700

    Spot-checks on ChemProt.

--- dw-experiments/2020-06-15/script/01_train_model.sh
+++ dw-experiments/2020-06-15/script/01_train_model.sh
@@ -13,7 +13,7 @@ ie_train_data_path=$data_root/train.json \
     ie_dev_data_path=$data_root/dev.json \
     ie_test_data_path=$data_root/test.json \
     cuda_device="$cuda_device" \
-    allennlp train $config_file \
+    allennlp train --recover $config_file \
     --cache-directory $data_root/cached \
     --serialization-dir $model_dir/$experiment_name \
     --include-package dygie
--- dw-experiments/2020-06-15/script/03_examine.py
+++ dw-experiments/2020-06-15/script/03_examine.py
@@ -1,15 +1,26 @@
 import json
+from collections import Counter
 
 from dygie.data.dataset_readers.data_structures import Dataset
 
 
-data = [json.loads(line) for line in open("results/covid_anno_par_mech_effect_test.json")]
+data = [json.loads(line) for line in open("results/covid_abstracts.json")]
+
 
 n_predicted = 0
 
+
+counts = Counter()
+
+data = Dataset("results/covid_abstracts.json")
 for entry in data:
-    assert len(entry["sentences"]) == len(entry["predicted_relations"]) == 1
-    n_predicted += len(entry["predicted_relations"][0])
+    for sent in entry:
+        if sent.predicted_relations:
+            counts["ner"] += len(sent.predicted_ner)
+            counts["relations"] += len(sent.predicted_relations)
+            print(sent)
+            for pred in sent.predicted_relations:
+                print(f"- {pred}")
+            print("\n" + 20 * "#" + "\n")
 
-
-print(n_predicted)
+print(counts)
