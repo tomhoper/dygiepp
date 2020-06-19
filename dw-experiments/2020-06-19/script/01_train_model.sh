296c8f33d8aba07fedc8757ffb769723216e9bfb
Dave Wadden <dwadden@cs.washington.edu>
2020-06-19 12:54:35 -0700

    Experiment to check on Aida's issues with `covid_anno_par`.

---
+++ dw-experiments/2020-06-19/script/01_train_model.sh
@@ -0,0 +1,19 @@
+experiment_name="covid_anno_par_mech_effect"
+
+model_dir=$PWD/models
+dygie_dir=../..
+
+cd $dygie_dir
+data_root="./data/UnifiedData/covid_anno_par/mapped/mech_effect"
+config_file="./training_config/chemprot.jsonnet"
+cuda_device=0,1
+
+# Train model.
+ie_train_data_path=$data_root/train.json \
+    ie_dev_data_path=$data_root/dev.json \
+    ie_test_data_path=$data_root/test.json \
+    cuda_device="$cuda_device" \
+    allennlp train $config_file \
+    --cache-directory $data_root/cached \
+    --serialization-dir $model_dir/$experiment_name \
+    --include-package dygie
