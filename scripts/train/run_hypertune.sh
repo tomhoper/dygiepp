c7b4cd3b8a1359f33a7154ce877033ecee062100
Math-QA <math.qa.dataset@gmail.com>
2020-07-24 16:34:48 -0700

    number of cpus added

--- scripts/train/run_hypertune.sh
+++ scripts/train/run_hypertune.sh
@@ -1,8 +1,8 @@
 python dygie/data/merge_corpus.py --root $1 --dataset_list $2
 if [ $4 = "mech" ]; then
   echo "mech only mode"
-  python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $4 --device 0,1,2,3 --num_samples $3
+  python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $5 --cpu_count $6  --num_samples $3
 else
   echo "effect and mech mode"
-  python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $4 --cpu_count $5 --device 0,1,2,3 --num_samples $3
+  echo python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $5 --cpu_count $6 --num_samples $3
 fi
