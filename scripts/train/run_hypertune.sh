82aebb891a7d2ec7e43ecd319f54fd637ab96349
Math-QA <math.qa.dataset@gmail.com>
2020-07-24 17:26:34 -0700

    number of cpus added

--- scripts/train/run_hypertune.sh
+++ scripts/train/run_hypertune.sh
@@ -4,5 +4,5 @@ if [ $4 = "mech" ]; then
   python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $5 --cpu_count $6  --num_samples $3
 else
   echo "effect and mech mode"
-  echo python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $5 --cpu_count $6 --num_samples $3
+  python scripts/train/train_allentune.py --root $1 --data_combo $2 --mech_effect_mode --gpu_count $5 --cpu_count $6 --num_samples $3
 fi
