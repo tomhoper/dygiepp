ac15284481a6410ae49aa1ba54506adc0db8c038
Math-QA <math.qa.dataset@gmail.com>
2020-08-02 01:00:07 -0700

    prodigy_lib

--- bio_annotations/command.sh
+++ bio_annotations/command.sh
@@ -11,3 +11,76 @@ prodigy rel.manual ner_rels_bio_jeff blank:en ./bio_selected6.jsonl --label USED
 prodigy rel.manual ner_rels_bio_megan blank:en ./bio_selected7.jsonl --label USED,EFFECT,DO --span-label ENTITY --host 3060
 
 prodigy rel.manual ner_rels_bio_n1 blank:en ./bio_selected0.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY --host 5000
+
+
+prodigy rel.manual ner_rels_bio_yeal blank:en ./bio_selected5.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY --host 4040
+prodigy rel.manual ner_rels_bio_sara blank:en ./bio_selected2.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+
+--validation
+prodigy rel.manual madeline_validation blank:en ./validate.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+madeline_correction --> port 3010
+prodigy rel.manual ner_rels_bio_madeline_new blank:en ./correction_annotations_madeline.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+
+
+
+megan_correction --> port 3020
+prodigy rel.manual ner_rels_bio_megan_new blank:en ./correction_annotations_megan.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+
+
+sara_correction --> port 3030
+prodigy rel.manual ner_rels_bio_sara_new blank:en ./correction_annotations_sara.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+
+
+prodigy rel.manual ner_rels_bio_tom_test_sara blank:en ./correction_annotations_sara.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+
+sara annotations --> port 5080
+prodigy rel.manual ner_rels_bio_sara_v2 blank:en ./bio_selected4.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+yeal annotations --> port 5060
+prodigy rel.manual ner_rels_bio_yeal blank:en ./bio_selected10.jsonl --label USED,EFFECT,DO --span-label ENTITY
+##################################################################### bad az kharab kari
+SARA:
+annotation:  port 5080
+prodigy rel.manual ner_rels_bio_sara blank:en ./bio_selected4.jsonl --label USED,EFFECT,DO --span-label ENTITY
+correction: port 3030 :
+prodigy rel.manual ner_rels_bio_sara_correction blank:en correction_input_sara.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+MEGAN
+annotation:  port 3060
+prodigy rel.manual ner_rels_bio_megan blank:en ./bio_selected7.jsonl --label USED,EFFECT,DO --span-label ENTITY 
+correction:  3020
+prodigy rel.manual ner_rels_bio_megan_correction blank:en correction_input_megan.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+yeal  
+annotation : 5060
+prodigy rel.manual ner_rels_bio_yeal blank:en ./bio_selected10.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+MADELINE
+annotation : 5050
+prodigy rel.manual ner_rels_bio_madeline blank:en ./bio_selected11.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+correction:  3010
+prodigy rel.manual ner_rels_bio_madeline_correction blank:en correction_input_madeline.jsonl --label USED,EFFECT,DO --span-label ENTITY
+validation 3000:
+prodigy rel.manual ner_rels_bio_madeline_correction blank:en validation_input_madeline.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
+
+
+TOM
+annotations : 1111
+prodigy rel.manual ner_rels_bio_tom blank:en ./bio_selected12.jsonl --label USED-TO,EFFECT,DO --span-label ENTITY
+
+
+tom:
+prodigy rel.manual method_measure_tom blank:en ./human_annotations.jsonl --label USED-TO --span-label ENTITY
+curation annotations port 2222:
+prodigy rel.manual ner_rels_bio_tom_correction blank:en ./tom_curation_file.jsonl --label USED,EFFECT,DO --span-label ENTITY
+prodigy rel.manual tt blank:en ./tom_curation_file.jsonl --label USED,EFFECT,DO --span-label ENTITY
+
--- bio_annotations/make_annotation_data.py
+++ bio_annotations/make_annotation_data.py
@@ -1,6 +1,15 @@
 import json
 import random
+from os import listdir
+from os.path import isfile, join
+import pathlib
+from pathlib import Path
+import utils as ut
+
 random.seed(100)
+ANNOTATED_FILEPATH = "annotations/"
+NAME_LIST  = ["megan", "madeline", "kristina", "jeff", "sara", "yeal"]
+
 
 def get_info_from_data(data):
   output = {}
@@ -42,36 +51,95 @@ def get_info_from_data_partitioned(data):
   output_list.append(output)
   return output_list
 
+def check_alread_annotated(allread_annotated_list, datapoint):
+
+    if (datapoint['meta']['doc_key'], datapoint['text']) not in allread_annotated_list:
+      return True
+    return False
+
+
 def create_annotation_data(input_filename, output_filename):
-  # import pdb; pdb.set_trace()
-  input_file = open(input_filename)
-  
-  output_res = []
-  data_list = []
-  for line in input_file:
-    data_list.append(json.loads(line))
-  random.shuffle(data_list)
-  common_annotations = []
-  for i in range(1,10):
+    # ut.update_extractions(NAME_LIST, ANNOTATED_FILEPATH)
+    already_annotated = find_already_annotated()  
+    # anotator_already_annotated = find_already_annotated_by_annotator("sara")  
+    # anotator_already_annotated = find_already_annotated_by_annotator("megan")  
+    anotator_already_annotated = find_already_annotated_by_annotator("yeal")  
+    # anotator_already_annotated = find_already_annotated_by_annotator("madeline")  
+    annotator_abstract_id_text = []
+    print(len(anotator_already_annotated))
+    already_annotated_abstract_id_text = []
+
+    for item in already_annotated:
+      already_annotated_abstract_id_text.append((item['meta']['doc_key'], item['text']))
+
+    for item in anotator_already_annotated:
+      annotator_abstract_id_text.append((item['meta']['doc_key'], item['text']))
+
+    input_file = open(input_filename)    
     output_res = []
-    # if i > 1:
-    #   for r in common_annotations:
-    #     output_res.append(r)
-    print(i)
-    output_file = open(output_filename + str(i) + '.jsonl', 'w')
-    data_list_part = data_list[i*100:(i+1)*100]
-    for j in range(len(data_list_part)):
-      data = data_list_part[j]
-      res = get_info_from_data_partitioned(data)
-      for r in res:
-          # import pdb; pdb.set_trace()
-          # if j < 10 and i == 1:
-          #   common_annotations.append(r)
-          output_res.append(r)
-    # data_list.append(get_info_from_data(data))
-    for data in output_res:
-      json.dump(data, output_file)
-      output_file.write("\n")
+    data_list = []
+
+    for line in input_file:
+      data_list.append(json.loads(line))
+    
+    random.shuffle(data_list)
+    common_annotations = []
+    common_data = []
+    for i in range(0,10):
+      output_res = []
+      
+
+      print(i)
+      output_file = open(output_filename + str(i) + '.jsonl', 'w')
+      data_list_part = data_list[i*100:(i+1)*100]
+      for j in range(len(data_list_part)):
+        data = data_list_part[j]
+        res = get_info_from_data_partitioned(data)
+        for r in res:
+            # import pdb; pdb.set_trace()
+            if j < 10 and i == 1:
+              # import pdb; pdb.set_trace()
+              if check_alread_annotated(annotator_abstract_id_text, r) == False:
+                print("here")
+                continue
+              common_annotations.append(r)
+            output_res.append(r)
+      common_data_written = False
+      for data in output_res:
+        if check_alread_annotated(already_annotated_abstract_id_text, data) == False:
+          continue
+        if common_data_written == False and i > 1:
+          common_data_written = True
+          for d in common_annotations:
+            json.dump(d, output_file)
+            output_file.write("\n")
+        json.dump(data, output_file)
+        output_file.write("\n")
+
+    # import pdb; pdb.set_trace()
+    for i in range(10,20):
+      output_res = []
+      print(i)
+      output_file = open(output_filename + str(i) + '.jsonl', 'w')
+      data_list_part = data_list[i*200:(i+1)*200]
+      for j in range(len(data_list_part)):
+        data = data_list_part[j]
+        res = get_info_from_data_partitioned(data)
+        for r in res:
+            # import pdb; pdb.set_trace()
+            output_res.append(r)
+      common_data_written = False
+      for data in output_res:
+        if check_alread_annotated(already_annotated_abstract_id_text, data) == False:
+          continue
+        if common_data_written == False and i > 1:
+          common_data_written = True
+          for d in common_annotations:
+
+            json.dump(d, output_file)
+            output_file.write("\n")
+        json.dump(data, output_file)
+        output_file.write("\n")  
 
 def find_the_val_train_index_list(input_filename):
   input_file = open(input_filename)
@@ -91,11 +159,35 @@ def find_the_val_train_index_list(input_filename):
     json.dump(data_list[i], output_train_file)
     output_train_file.write('\n')
     
+def find_already_annotated():
+    res_list = []
+    annotation_path =  pathlib.Path(ANNOTATED_FILEPATH) / "jsons"  
+    onlyfiles = [f for f in listdir(str(annotation_path)) if (isfile(join(str(annotation_path), f)) and f.endswith('jsonl'))]
+    for file in onlyfiles:
+        print(file)
+        annotator_name = file.split('.')[0]
+        res_list = res_list + ut.read_data_base(join(str(annotation_path), file), annotator_name)
+    return res_list
+
+def find_already_annotated_by_annotator(name):
+    res_list = []
+    annotation_path =  pathlib.Path(ANNOTATED_FILEPATH) / "jsons"  
+    onlyfiles = [f for f in listdir(str(annotation_path)) if (isfile(join(str(annotation_path), f)) and f.endswith('jsonl'))]
+    for file in onlyfiles:
+        if name not in file:
+          continue
+        print(file)
+        annotator_name = file.split('.')[0]
+        res_list = res_list + ut.read_data_base(join(str(annotation_path), file), annotator_name)
+    return res_list
 
 
-# create_annotation_data("../dygiepp/data/covid/json/test_abstracts_50k_valid_removed.json", "bio_selected_t")
 
-find_the_val_train_index_list("../dygiepp/data/covid/json/test_abstracts_50k_valid_removed.json")
+
+
+create_annotation_data("../data/covid/json/test_abstracts_50k_valid_removed.json", "bio_selected")
+
+# find_the_val_train_index_list("../dygiepp/data/covid/json/test_abstracts_50k_valid_removed.json")
 
 
 
