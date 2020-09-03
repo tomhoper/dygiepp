bd2baabbf37a433b66622f8b6722fde17016ddc1
Dave Wadden <dwadden@cs.washington.edu>
2020-09-03 16:27:30 -0700

    Make coref predictions on Aida's full dataset.

---
+++ dw-experiments/2020-09-03/script/02_predict.py
@@ -0,0 +1,33 @@
+import subprocess
+import multiprocessing
+import os
+
+model_dir = "/data/dwadden/proj/dygie/dygiepp-new/models"
+
+
+def predict_shard(input_dict):
+    shard_id = input_dict["shard_id"]
+    model_name = input_dict["model_name"]
+    cmd = ["allennlp", "predict",
+           f"{model_dir}/{model_name}/model.tar.gz",
+           f"data/shards/shard-{shard_id}.jsonl",
+           "--predictor", "dygie",
+           "--include-package", "dygie",
+           "--use-dataset-reader",
+           "--output-file", f"results/predictions/{shard_id}-{model_name}.jsonl",
+           "--cuda-device", str(shard_id - 1)]
+
+    stdout_file = f"log/stdout-{shard_id}-{model_name}.log"
+    stderr_file = f"log/stderr-{shard_id}-{model_name}.log"
+
+    with open(stdout_file, "w", buffering=1) as f_stdout, \
+            open(stderr_file, "w", buffering=1) as f_stderr:
+        subprocess.run(cmd, stdout=f_stdout, stderr=f_stderr)
+
+
+####################
+
+# os.rmdir("cache")
+workers = multiprocessing.Pool(4)
+inputs = [{"shard_id": i + 1, "model_name": "scierc"} for i in range(4)]
+workers.map(predict_shard, inputs)
